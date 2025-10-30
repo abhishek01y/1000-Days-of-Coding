1823df33707ea017923abe1d20d5a6e33ff7b0d8
abhishek01y <abhishkeyadav5@gmail.com>
2025-10-30 23:34:04 +0530

    Added basic C++ programs: power, table, factorial, sum, and prime number
    Uploaded five C++ files that perform basic mathematical operations:
    1. power.cpp - calculates power of a number
    2. table.cpp - prints multiplication table
    3. factorial.cpp - finds factorial
    4. sum.cpp - calculates sum of n numbers
    5. prime.cpp - checks if a number is prime

---
+++ sum.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i,n,sum=0;
+    cout<<"Enter total number sum u want: ";
+    cin>>n;
+    for(i=1;i<n;i++)
+    {
+        sum +=i;
+    }
+    cout<<sum;
+    return 0;
+}
---
+++ table.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i;
+    for(i=1;i<=10;i++)
+    {
+        cout<<"3 * "<<i<<" = "<<3*i;
+        cout<<"\n";
+
+    }
+    return 0;
+}

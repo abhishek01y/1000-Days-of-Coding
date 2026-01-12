bdbd8756c0d7154584c1a7e999bd8564b838e6b5
abhishek01y <abhishkeyadav5@gmail.com>
2026-01-12 22:51:32 +0530

    Add files via upload

---
+++ Day 21/max.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int>v(n);
+    for(int i=0;i<n;i++)
+    {
+        cin>>v[i];
+    }
+    int mx = *max_element(v.begin(),v.end());
+    cout<<mx;
+
+    return 0;
+}

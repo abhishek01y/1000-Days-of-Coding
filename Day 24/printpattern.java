d6bbd41cc997715720990452a42ebc06e8852098
abhishek01y <abhishkeyadav5@gmail.com>
2026-03-31 21:42:18 +0530

    Add files via upload

---
+++ Day 24/printpattern.java
@@ -0,0 +1,8 @@
+class printpattern{
+    public static void main(String[] args){
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+    }
+}
